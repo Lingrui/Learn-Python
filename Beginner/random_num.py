8addfddf1ec3f6ca2bde55e4ece51cb1b7346478
Lingrui <lingruicai@gmail.com>
2017-09-10 23:05:36 -0400

    update

---
+++ Beginner/random_num.py
@@ -0,0 +1,40 @@
+#!/user/bin/python 
+##random number between 0 and 1 
+from random import * 
+print random()
+
+#generate a random number between 1 and 100
+from random import * 
+print randint(1,100)  ##pick a random number between 1 and 100 
+
+#store it in a vairable 
+from random import * 
+x = randint(1,100)
+print x
+
+##random number between 1 and 10 
+from random import * 
+print uniform(1,10)
+
+##fun with list 
+#shuffle a list 
+from random import * 
+items = [1,2,3,4,5,6,7,8,9,10]
+shuffle(items)
+print items 
+
+#pick a random number from a list 
+from random import * 
+items = [1,2,3,4,5,6,7,8,9,10]
+x = sample(items, 1)
+print x[0]
+y = sample(items,4)
+print y 
+
+## same thing with a list of strings 
+items = ['Alissa','Alice','Marco','Melissa','Sandra','Steve']
+x = sample(items,1)
+print x[0]
+
+y = sample(items,2)
+print y
---
+++ Beginner/test.py
@@ -0,0 +1,10 @@
+#!/user/bin/python
+class Employee:
+	def __init__(self,name,salary):
+		self.salary = salary
+		self.name = name
+
+james = Employee("James","1200")
+
+print (james.name)
+print (james.salary)
