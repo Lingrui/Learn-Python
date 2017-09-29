f7e23a828626490e359856f7fa9786a2a3eac7da
Lingrui <lingruicai@gmail.com>
2017-09-29 17:30:58 -0400

    cookbook

--- cookbook/1.9.py
+++ cookbook/1.9.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python 
+a = {
+	'x':1,
+	'y':2,
+	'z':3
+}
 
+b = {
+	'w':10,
+	'x':11,
+	'y':2
+}
+
+#Find keys in common
+#a.keys() & b.keys() #{'x','y'}
+
+c = {key:a[key] for key in a.keys()-{'z','w'}}
